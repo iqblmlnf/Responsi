f43dd0bd3d73b94a0145c7372b03cb6f3e3108c4
iqblmlnf <iqbalmaulanafajar36@gmail.com>
2025-07-11 16:47:58 +0700

    chore: setup Vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,19 +1,10 @@
+const express = require('express');
 const { educationHistory, skills, projects } = require('../backend/data');
+const app = express();
 
-module.exports = (req, res) => {
-  const { url, method } = req;
+app.get('/api/education', (req, res) => res.json(educationHistory));
+app.get('/api/skills', (req, res) => res.json(skills));
+app.get('/api/projects', (req, res) => res.json(projects));
 
-  if (url === '/api/education') {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).end(JSON.stringify(educationHistory));
-  } else if (url === '/api/skills') {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).end(JSON.stringify(skills));
-  } else if (url === '/api/projects') {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).end(JSON.stringify(projects));
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found');
-  }
-};
+// HARUS: export sebagai handler untuk Vercel!
+module.exports = app;
