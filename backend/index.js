ae295f509b69da47f1e9bd3dc750336a2e25fc13
iqblmlnf <iqbalmaulanafajar36@gmail.com>
2025-07-11 16:24:40 +0700

    chore: setup Vercel deployment

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const { educationHistory, skills, projects } = require('./data');
 
 const app = express();
-const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +11,6 @@ app.get('/api/education', (req, res) => res.json(educationHistory));
 app.get('/api/skills', (req, res) => res.json(skills));
 app.get('/api/projects', (req, res) => res.json(projects));
 
-app.listen(PORT, () => {
-console.log(` Server backend berjalan di http://localhost:${PORT}`);
-});
+// Jangan pakai app.listen()!
+// Ganti dengan export handler
+module.exports = app;
