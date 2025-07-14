5f2e24ecb92c0bc1e89e8dcb54863013cc8eb029
iqblmlnf <iqbalmaulanafajar36@gmail.com>
2025-07-14 14:44:55 +0700

    update lagi dan lagi

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';  // Hanya import HomeView.vue
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
-    }
-    // Hapus route yang mengarah ke AboutView.vue
-  ]
-});
-
-export default router;
+      component: HomeView,
+    },
+  ],
+})
+export default router
